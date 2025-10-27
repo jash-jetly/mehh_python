a0a144c5c01664954ada5bfa95462188ca9c3231
Jash Jetly <jashjetly@gmail.com>
2025-10-27 14:50:54 +0530

    addition

--- lec_1/lec_1.2.py
+++ lec_1/lec_1.2.py
@@ -2,3 +2,8 @@ name = input("Please enter your name: ")
 age = input("Please enter your age: ")
 
 print(f"my name is {name} and my age is {age}")
+
+num1 = int(input("1st: "))
+num2 = int(input("2nd: "))
+
+print(num1+num2)
